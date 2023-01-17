f2938874ae01617477a862cb30d76839adaf7f2f
nick <nicholasbaird@proton.me>
2023-01-17 13:46:32 -0600

    commit modal complete

--- web/src/components/CloseButton/CloseButton.tsx
+++ web/src/components/CloseButton/CloseButton.tsx
@@ -7,20 +7,7 @@ interface Props {
 function CloseButton({ onClick }: Props) {
   return (
     <div className="close-button" onClick={() => onClick()}>
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        fill="#000"
-        viewBox="0 0 24 24"
-        strokeWidth="1.5"
-        stroke="currentColor"
-        className="w-6 h-6"
-      >
-        <path
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          d="M6 18L18 6M6 6l12 12"
-        />
-      </svg>
+      Close
     </div>
   );
 }
--- web/src/components/RepoModal/RepoModal.tsx
+++ web/src/components/RepoModal/RepoModal.tsx
@@ -1,6 +1,9 @@
+import { useState, useEffect } from 'react';
+import ReactMarkdown from 'react-markdown';
 import { Repo } from '../../../types/Repo';
 import { CloseButton } from '../CloseButton/CloseButton';
 import './RepoModal.css';
+import { Commit } from '../../../types/Commit';
 
 interface Props {
   repo: Repo;
@@ -8,13 +11,49 @@ interface Props {
 }
 
 function RepoModal({ repo, closeModal }: Props) {
+  const [commit, setCommit] = useState<Commit>();
+  const [readme, setReadme] = useState<string>();
+
+  // get most recent commit and readme
+  useEffect(() => {
+    const getCommit = async () => {
+      const res = await fetch(repo.commits_url.replace('{/sha}', ''));
+      if (res.ok) {
+        const data = await res.json();
+        const sortedCommits = data.sort(
+          (a: Commit, b: Commit) =>
+            Date.parse(b.commit.author.date) - Date.parse(a.commit.author.date)
+        );
+
+        setCommit(sortedCommits[0]);
+      }
+    };
+
+    const getReadme = async () => {
+      const res = await fetch(
+        `https://raw.githubusercontent.com/${repo.full_name}/master/README.md`
+      );
+      if (res.ok) {
+        if (res.body) {
+          const data = await res.text();
+          console.log(data);
+          setReadme(data);
+        }
+      }
+    };
+
+    getCommit();
+    getReadme();
+  }, [repo.commits_url, repo.full_name]);
+
   return (
     <div className="repomodal-container">
       <div className="repomodal">
         <CloseButton onClick={closeModal} />
-        <div>Most recent commit date: </div>
-        <div>Author: </div>
-        <div>Message: </div>
+        <div>Most recent commit date: {commit?.commit.author.date}</div>
+        <div>Author: {commit?.commit.author.name}</div>
+        <div>Message: {commit?.commit.message}</div>
+        <div>{readme && <ReactMarkdown>{readme}</ReactMarkdown>}</div>
       </div>
     </div>
   );
